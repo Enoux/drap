d9c557bfd971b39e63091192af2140566cbaf200
Eriene Galinato <edgalinato@up.edu.ph>
2026-04-02 16:57:16 +0800

    feat(`PreferredLab`): impose badge height

--- src/lib/users/preferred-lab.svelte
+++ src/lib/users/preferred-lab.svelte
@@ -10,7 +10,7 @@
 
 <div class="overflow-x-auto">
   {#each labs as lab (lab)}
-    <Badge variant="outline" class="mr-1 border-accent bg-accent/10 text-xs uppercase">
+    <Badge variant="outline" class="mr-1 border-accent bg-accent/10 text-xs uppercase h-fit py-1 px-2">
       {lab}
     </Badge>
   {/each}
