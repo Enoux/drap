d83b232896daf5bea4120820b60a71074ccfd5f6
Eriene Galinato <edgalinato@up.edu.ph>
2026-04-02 21:58:26 +0800

    chore: run formatter

--- src/routes/dashboard/(draft)/drafts/[draftId]/+page.server.ts
+++ src/routes/dashboard/(draft)/drafts/[draftId]/+page.server.ts
@@ -101,23 +101,18 @@ export async function load({ params, locals: { session } }) {
       error(404);
     }
 
-    const {
-      studentCount,
-      quotaSnapshots,
-      allowlistCount,
-      lateRegistrantsCount,
-      timelineData,
-    } = await db.transaction(
-      // Needs to be done sequentially because parallel queries in a transaction are not supported.
-      async db => ({
-        studentCount: await getStudentCountInDraft(db, draftId),
-        quotaSnapshots: await getDraftLabQuotaSnapshots(db, draftId),
-        allowlistCount: await getAllowlistCountByDraft(db, draftId),
-        lateRegistrantsCount: await getLateRegistrantsCountByDraft(db, draftId),
-        timelineData: await getDraftRegistrationTimeline(db, draftId),
-      }),
-      { isolationLevel: 'repeatable read' },
-    );
+    const { studentCount, quotaSnapshots, allowlistCount, lateRegistrantsCount, timelineData } =
+      await db.transaction(
+        // Needs to be done sequentially because parallel queries in a transaction are not supported.
+        async db => ({
+          studentCount: await getStudentCountInDraft(db, draftId),
+          quotaSnapshots: await getDraftLabQuotaSnapshots(db, draftId),
+          allowlistCount: await getAllowlistCountByDraft(db, draftId),
+          lateRegistrantsCount: await getLateRegistrantsCountByDraft(db, draftId),
+          timelineData: await getDraftRegistrationTimeline(db, draftId),
+        }),
+        { isolationLevel: 'repeatable read' },
+      );
 
     const labs = quotaSnapshots.map(({ labId, labName, initialQuota }) => ({
       id: labId,
